ea3998f2f654be25a11cb32d752b3b96df3f8ec8
Lance Easley <leasley21@basecampcodingacademy.org>
2021-03-31 10:55:04 -0500

    Completed all rubric points

--- script.js
+++ script.js
@@ -3,16 +3,61 @@ const MAX_SIZE = 20;
 const MIN_DURATION = 2000;
 const MAX_DURATION = 5000;
 
+const snowVsRainInput = document.getElementById("snow-vs-rain");
+const quantityInput = document.getElementById("quantity");
+const colorInput = document.getElementById("click-color");
+const windInput = document.getElementById("wind");
+
 const snowflakesContainer = document.getElementById("snowflakes-container");
 
-setInterval(() => createSnowflake(), 50);
+let rainAmount = 0.5;
+let quantityAmount = 400;
+let colorValue = "#A600FF";
+let windAmount = 0;
+
+let repeatingElement = setInterval(() => createSnowflake(), quantityAmount);
+
+snowVsRainInput.addEventListener("input", () => {
+  rainAmount = Number(snowVsRainInput.value)
+})
+
+quantityInput.addEventListener("input", () => {
+  switch (quantityInput.value) {
+    case "0":
+      quantityAmount = 1000000;
+      break;
+    case "1":
+      quantityAmount = 400;
+      break;
+    case "2":
+      quantityAmount = 100;
+      break;
+    case "3":
+      quantityAmount = 25;
+      break;
+    case "4":
+      quantityAmount = 0;
+      break;
+  }
+  clearInterval(repeatingElement);
+  repeatingElement = setInterval(() => createSnowflake(), quantityAmount);
+})
+
+colorInput.addEventListener("input", () => {
+  colorValue = colorInput.value
+})
+
+windInput.addEventListener("input", () => {
+  windAmount = windInput.value
+})
+
 
 function randint(lo, hi) {
   return Math.random() * (hi - lo) + lo;
 }
 
 function randomIcon() {
-  if (Math.random() < 0.5) {
+  if (Math.random() > rainAmount) {
     return "fa-snowflake";
   } else {
     return "fa-tint";
@@ -23,6 +68,7 @@ function createSnowflake() {
   const snowFlake = document.createElement("i");
 
   snowFlake.classList.add("fas", randomIcon());
+  snowFlake.style.color = colorValue;
   snowFlake.style.left = randint(0, 100) + "%";
   snowFlake.style.opacity = Math.random();
   snowFlake.style.fontSize = randint(MIN_SIZE, MAX_SIZE) + "px";
@@ -31,7 +77,7 @@ function createSnowflake() {
 
   snowFlake
     .animate(
-      { transform: `translate(0vw, 100vh)` },
+      { transform: `translate(${windAmount}vw, 100vh)` },
       { duration: randint(MIN_DURATION, MAX_DURATION) }
     )
     .finished.then(() => snowFlake.remove());
